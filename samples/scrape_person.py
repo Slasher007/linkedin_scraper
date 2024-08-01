a5c9e3b6f333a5cbab87cac05fcbcd76f3f18441
Slasher007 <mamadou.kebe.edu@groupe-gema.com>
2024-08-01 19:00:54 +0200

    Update scrape_person.py

--- samples/scrape_person.py
+++ samples/scrape_person.py
@@ -3,6 +3,9 @@ from linkedin_scraper import actions
 from driver_setup import setup_driver
 import time
 import pandas as pd
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 
 linked_user = "xorover503@qodiq.com"
 
@@ -37,18 +40,75 @@ def get_company_people_details(driver, company_url):
     scroll_to_bottom(driver)
 
 
+def accept_cookies(driver):
+
+    # find all buttons
+    buttons = driver.find_elements(By.TAG_NAME, 'button')
+
+    # Check button where text is "Tout accepter"
+    for button in buttons:
+        button_text = button.text
+        button_text = button_text.lower()
+        if 'accepter' in button_text:
+            button.click()
+            break
+
+
+def search_company_employees(driver, company_name):
+    # Send keys in search bar "{company_name} LinkedIn employees"
+    search_bar = driver.find_element(By.XPATH, '//textarea[@title="Rechercher"]')
+    search_bar.send_keys(f"{company_name} linkedin employees", Keys.ENTER)
+    time.sleep(2)
+
+
+def get_google_search_results(driver):
+    all_a_tag = driver.find_elements(By.TAG_NAME, 'a')
+
+    for a_tag in all_a_tag:
+        href = a_tag.get_attribute('href')
+        if href and '/in/' in href:
+            print(href)
+
+
 if __name__ == "__main__":
+    # Setup driver
     driver = setup_driver()
 
+    # Load creds
     email = os.getenv("LINKEDIN_USER")
     password = os.getenv("LINKEDIN_PASSWORD")
 
     # Load company list
     df_companies = pd.read_csv('company.csv')
 
-    # Login LinkedIn
-    actions.login(driver, email, password)
+    # Go in google search
+    driver.get('https://www.google.com')
+    time.sleep(2)
+
+    # Accept cookies
+    accept_cookies(driver)
+    time.sleep(2)
+
+    #
 
     for index, row in df_companies.iterrows():
-        time.sleep(5)
-        get_company_people_details(driver, row['linked_url'])
+
+        # Search company employees on Google
+        search_company_employees(driver, row['name'])
+
+        # Get employees details
+        get_google_search_results(driver)
+
+        #
+
+
+
+
+
+    # Login LinkedIn
+    #actions.login(driver, email, password)
+
+    #for index, row in df_companies.iterrows():
+
+    #    time.sleep(5)
+    #    get_company_people_details(driver, row['linked_url'])
